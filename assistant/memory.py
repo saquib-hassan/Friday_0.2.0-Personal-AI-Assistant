8dad2db1d9cd4c4a1c6c5d9ae64e5d1095c8befa
Saquib Hassan <hsakib1244@outlook.com>
2025-12-28 13:00:39 +0600

    memory functionality in constructor

--- assistant/memory.py
+++ assistant/memory.py
@@ -1,6 +1,16 @@
+import os
+import json
+
 class Memory:
     def __init__(self):
-        pass
+        self.file_path = "H://Friday_0.2.0-Personal-AI-Assistant//assistant//memory.json"
+        self.history = []
+        if os.path.exists(self.file_path):
+            with open(self.file_path,"r") as j:
+               self.history= json.load(j)
+        else:
+            self.history = []
+
     
     def add(self,role,message):
         pass
