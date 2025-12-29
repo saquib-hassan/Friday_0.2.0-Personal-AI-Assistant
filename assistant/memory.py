4c41479fe39583f78761c17798733394080a3fa2
Saquib Hassan <hsakib1244@outlook.com>
2025-12-29 11:13:07 +0600

    Adds a message to memory and saves it to JSON

--- assistant/memory.py
+++ assistant/memory.py
@@ -13,7 +13,21 @@ class Memory:
 
     
     def add(self,role,message):
-        pass
+        """
+        Docstring for add
+        
+        :param self: It expects an obeject
+        :param role: User role
+        :param message: I've created a message dictionary and appened it to history list and 
+                        open json as write mode and save the history later.
+        """
+        self.message = {"role":role, "message": message}
+        self.history.append(self.message)
+
+        with open(self.file_path,"w") as w:
+            json.dump(self.history,w)
+
+        
 
     def get_history(self):
         pass
